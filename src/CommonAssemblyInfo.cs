2ac6eb2f539088cd946dae8040405923433e1262
hazzik <hazzik@gmail.com>
2011-06-17 17:14:52 +0600

    Added common assembly info

---
+++ src/CommonAssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("OoMapper")]
+[assembly: AssemblyCopyright("Copyright © hazzik 2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
